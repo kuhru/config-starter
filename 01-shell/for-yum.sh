e45db8128a7465e328977785a1df9061f1bb2e2c
Alekh Karn <karnalekh@gmail.com>
2024-04-01 03:13:22 +0530

    some cleanup for-yum around shell setting

--- 01-shell/for-yum.sh
+++ 01-shell/for-yum.sh
@@ -6,19 +6,23 @@ yum upgrade -y
 
 # basic setup
 
-yum install -y curl-minimal wget git nano
-# git clone https://github.com/kuhru/setup-everything.git
+yum install -y curl-minimal wget git nano which
+# mkdir ~/Documents && cd ~/Documents && git clone https://github.com/kuhru/setup-everything.git && cd setup-everything
+# nano 01-shell/for-yum.sh
+# ./01-shell/for-yum.sh
 
 # zsh and omz download
 
-yum install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+yum install -y zsh util-linux-user
+CHSH=no RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh) && echo ""
+export SHELL=$(which zsh)
 
 # zsh download needed plugins
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && echo ""
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && echo ""
 
 # omz setup
 
-cat ./01-shell-_zshrc > ~/.zshrc
+cat ./01-shell/_zshrc_dump > ~/.zshrc
